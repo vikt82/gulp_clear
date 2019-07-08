a0e994d0a6dc7cf9381e4fe9e980bf3b5bcf3789
Viktor Puchkov <victor.p@wecandevelopit.com>
2019-07-08 17:32:56 +0300

    [add] gulp data, fs

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,22 @@
 var gulp = require('gulp');
 
 // template
-var pug = require('gulp-pug');
+var pug = require('gulp-pug'),
+    data = require('gulp-data'),
+    fs = require('fs');
 
 // Clear
 var del = require('del');
 
 // TEMPLATE
 gulp.task('pug', function() {
-  return gulp.src('src/template/*.pug')
+  return gulp.src('./src/template/*.pug')
+  .pipe(data(function(file) {
+    return {
+      data: JSON.parse(fs.readFileSync('./src/template/data/data.json', 'utf8')),
+      navigation: JSON.parse(fs.readFileSync('./src/template/data/navigation.json', 'utf8'))
+    }
+  }))
   .pipe(pug({
     pretty: true
   }))
